2851d5b27e745e6d472e694665560c2b3743bae7
Bruna Ceppa <87783719+Bcpp16@users.noreply.github.com>
2022-10-17 20:21:33 -0300

    Detalles

--- js/index.js
+++ js/index.js
@@ -1,30 +1,5 @@
-let modal = document.getElementById("myModal");
+// BTN MENU HAMBURGUESA ==============================================
 
-// Get the button that opens the modal
-let btn = document.getElementById("btnModal");
-
-// Get the <span> element that closes the modal
-let span = document.getElementsByClassName("close")[0];
-
-// When the user clicks the button, open the modal 
-btn.onclick = function() {
-  modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
-
-
-/* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
 function myFunction() {
   let x = document.getElementById("myLinks");
   if (x.style.display === "block") {
@@ -33,3 +8,28 @@ function myFunction() {
     x.style.display = "block";
   }
 }
+
+
+// MODAL ==============================================
+
+let modal = document.getElementById("myModal");
+let btn = document.getElementById("btnModal");
+let span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+
